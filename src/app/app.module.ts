35509f876c24e8675f271fe3307be8961f24552d
Alice Russell <alice_r1295@hotmail.com>
2020-12-31 03:51:31 -0600

    initial design of ide panels

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DefinitionsComponent } from './definitions/definitions.component';
+import { EvaluationsComponent } from './evaluations/evaluations.component';
+import { EditorComponent } from './editor/editor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DefinitionsComponent,
+    EvaluationsComponent,
+    EditorComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/definitions/definitions.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-definitions',
+  templateUrl: './definitions.component.html',
+  styleUrls: ['./definitions.component.scss']
+})
+export class DefinitionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
