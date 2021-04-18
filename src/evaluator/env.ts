8178e797812a32133afc63800b8a9d75edd734b1
Alice Russell <alice_r1295@hotmail.com>
2021-04-18 07:46:28 -0500

    Restructuring the builtin environment again, and adding some new values

--- src/evaluator/env.ts
+++ src/evaluator/env.ts
@@ -7,7 +7,7 @@
 'use strict';
 
 import { MakeStructType, MakeStructureConstructor, MakeStructureAccessor } from './constructors';
-import { ExprResult, Env, Just, Maybe, Value, StructType } from './types';
+import { ExprResult, Env, Just, Maybe, Value, StructType, Expr } from './types';
 import { BFn, NFn, ValErr, MakeJust } from './constructors';
 
 export const builtinEnv = (): Env => {
@@ -15,25 +15,104 @@ export const builtinEnv = (): Env => {
 
   const posnType: StructType = MakeStructType('posn', ['x', 'y']);
 
-  env.set('+',  BFnEnv(checkArityThenApply('+', constructReducableNumberOperation((acc, elem) => acc + elem, 0), 2, true)));
-  env.set('-',  BFnEnv(checkArityThenApply('-', constructReducableNumberOperation((acc, elem) => acc - elem, 0, true, true), 1, true)))
-  env.set('*',  BFnEnv(checkArityThenApply('*', constructReducableNumberOperation((acc, elem) => acc * elem, 1), 2, true)));
-  env.set('/',  BFnEnv(checkArityThenApply('/', constructReducableNumberOperation((acc, elem) => acc / elem, 0, true), 2, true)));
-
-  env.set('string-append',  BFnEnv(constructReducableStringOperation((a, b) => a.concat(b), '')));
-  
   env.set('pi', NFnEnv(Math.PI));
+  env.set('true', NFnEnv(true));
+  env.set('false', NFnEnv(false));
+
+  env.set('+', plus());
+  env.set('-', minus());
+  env.set('*', multiply());
+  env.set('/', divide());
+
+  env.set('string-append', stringAppend());
+  env.set('string=?', stringEquals());
+  // env.set('substring');
   
-  env.set('sin', BFnEnv(constructSingletonNumberOperation('sin', x => Math.sin(x))));
-  env.set('cos', BFnEnv(constructSingletonNumberOperation('cos', x => Math.cos(x))));
+  env.set('sin', sin());
+  env.set('cos', cos());
+  env.set('add1', addOne());
+  env.set('floor', floor());
+  // env.set('modulo');
+  // env.set('abs');
 
   env.set('make-posn', MakeJust(MakeStructureConstructor(posnType)));
   env.set('posn-x', MakeJust(MakeStructureAccessor(posnType, 0)));
   env.set('posn-y', MakeJust(MakeStructureAccessor(posnType, 1)));
   env.set('posn?', MakeJust(MakeStructureConstructor(posnType)));
 
+
+  // env.set('and');
+  // env.set('or');
+  // env.set('<');
+  // env.set('<=');
+  // env.set('number=?');
+  // env.set('>');
+  // env.set('>=');
+
+
   return env;
 }
+
+// ----------------------------------------------------------------------------
+
+// Environment function values.
+
+const plus = (): Just<ExprResult> => {
+  return BFnEnv(checkArityThenApply('+', constructReducableNumberOperation((acc, elem) => acc + elem, 0), 2, true));
+}
+
+const minus = (): Just<ExprResult> => {
+  return BFnEnv(checkArityThenApply('-', constructReducableNumberOperation((acc, elem) => acc - elem, 0, true, true), 1, true));
+}
+
+const multiply = (): Just<ExprResult> => {
+  return BFnEnv(checkArityThenApply('*', constructReducableNumberOperation((acc, elem) => acc * elem, 1), 2, true));
+}
+
+const divide = (): Just<ExprResult> => {
+  return BFnEnv(checkArityThenApply('/', constructReducableNumberOperation((acc, elem) => acc / elem, 0, true), 2, true));
+}
+
+const stringAppend = (): Just<ExprResult> => {
+  return BFnEnv(constructReducableStringOperation((a, b) => a.concat(b), ''));
+}
+
+const stringEquals = (): Just<ExprResult> => {
+  return BFnEnv(
+    checkArityThenApply(
+      'string=?', 
+      (vs: Value[]) => {
+        const first = vs[0]
+        if (first.type === 'NonFunction' && typeof first.value === 'string') 
+          return constructReducableStringOperation((acc, elem) => acc && (elem === first.value), true)(vs)
+        throw new Error('impossible line reached, bug in string=?');
+      },
+      2, 
+      true
+    )
+  );
+}
+
+const sin = (): Just<ExprResult> => {
+  return BFnEnv(constructSingletonNumberOperation('sin', x => Math.sin(x)));
+}
+
+const cos = (): Just<ExprResult> => {
+  return BFnEnv(constructSingletonNumberOperation('cos', x => Math.cos(x)));
+}
+
+const addOne = (): Just<ExprResult> => {
+  return BFnEnv(constructSingletonNumberOperation('add1', x => x + 1));
+}
+
+const floor = (): Just<ExprResult> => {
+  return BFnEnv(constructSingletonNumberOperation('floor', x => Math.floor(x)));
+}
+
+// ----------------------------------------------------------------------------
+
+
+// Helper functions for environment creation.
 
 const constructSingletonNumberOperation = (
   opName: string,
@@ -49,12 +128,29 @@ const constructSingletonNumberOperation = (
       return NFn(op(num));
     },
     1, false
-  )
+  );
+}
+
+const constructSingletonStringOperation = (
+  opName: string,
+  op: (x: string) => string,
+): ((vs: Value[]) => ExprResult) => {
+  return checkArityThenApply(
+    opName,
+    (vs: Value[]) => {
+      const strs = checkIfIsStrings(vs);
+      if (vs.length !== 1)
+        throw new Error('Impossible arity mismatch; fix constructSingletonStringOperation bug');
+      const str = strs[0];
+      return NFn(op(str));
+    },
+    1, false
+  );
 }
 
 const constructReducableNumberOperation = (
-  op: (a: number, b: number) => number,
-  id: number,
+  op: (a: any, b: number) => any,
+  id: any,
   idIsIndex?: boolean,
   subtraction?: boolean
 ): ((vs: Value[]) => ExprResult) => {
@@ -79,8 +175,8 @@ const constructReducableNumberOperation = (
 }
 
 const constructReducableStringOperation = (
-  op: (a: string, b: string) => string,
-  id: string
+  op: (a: any, b: string) => any,
+  id: any
 ): ((vs: Value[]) => ExprResult) => {
   return (vs: Value[]) => {
     const strings: string[] | false = checkIfIsStrings(vs);
