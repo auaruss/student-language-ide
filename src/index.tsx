0a5eb7b55c88af388340873f5e0a5274552b49b2
Alice Russell <alice@heysparkbox.com>
2021-10-13 07:40:06 -0500

    refactor: separate out App component and add components folder

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const App = () => (
-  <div>
-    <h1>
-      Hello World
-    </h1>
-  </div>
-);
+import App from './components/App';
 
 ReactDOM.render(<App />, document.getElementById('app'));
